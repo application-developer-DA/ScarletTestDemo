d1b72662d1f5af57c326dd42f0980684704a726d
Daniel Abramov <dabramov@snapview.de>
2019-02-25 16:26:19 +0100

    Add test `Toast` message

--- app/src/main/java/com/example/scarlettestdemo/MainActivity.kt
+++ app/src/main/java/com/example/scarlettestdemo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.scarlettestdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testMessage = "My Test Message"
+        val toast = Toast.makeText(this, testMessage, Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
